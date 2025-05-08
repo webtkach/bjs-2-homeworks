b90583954e284c3bfaf38216d79fff0542079724
Mike <web.tkach@gmail.com>
2025-05-08 17:28:18 +0300

    first

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = b ** -4 * a * c;
+
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		return [-b / (2 * a)];
+	} else {
+		return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let annualRate = percent / 100;
+	let monthRate = annualRate / 12;
+	let loanBody = amount - contribution;
+
+	let monthPay = loanBody * (monthRate + (monthRate / (((1 + monthRate) ** countMonths) - 1)));
+
+	let totalAmount = monthPay * countMonths;
+	totalAmount = Number(totalAmount.toFixed(2));
+
+	return totalAmount;
 }
