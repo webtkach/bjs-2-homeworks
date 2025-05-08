5c962c7b5517933464ea85c7a07f6ab3d0ec126b
Mike <web.tkach@gmail.com>
2025-05-08 17:58:05 +0300

    second

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,16 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let d = b ** -4 * a * c;
+	let d = b ** 2 - 4 * a * c;
 
 	if (d < 0) {
 		return [];
 	} else if (d === 0) {
 		return [-b / (2 * a)];
 	} else {
-		return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+		return [
+      (-b + Math.sqrt(d)) / (2 * a), 
+      (-b - Math.sqrt(d)) / (2 * a)];
 	}
 }
 
