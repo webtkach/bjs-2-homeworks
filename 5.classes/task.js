c34f281429fc4d1085983a6a41c4a086b9574c07
Mike <web.tkach@gmail.com>
2025-06-23 10:08:47 +0300

    Пятое задание полностью готово

---
+++ 5.classes/task.js
@@ -0,0 +1,94 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this._state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state = this.state * 1.5;
+	}
+
+	get state() {
+		return this._state;
+	}
+
+	set state(newState) {
+		if (newState < 0) {
+			this._state = 0;
+		} else if (newState > 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'magazine';
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = 'book';
+	}
+}
+
+class NovelBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = 'novel';
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = 'fantastic';
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = 'detective';
+	}
+}
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		for (let book of this.books) {
+			if (book[type] === value) {
+				return book;
+			}
+		}
+		return null;
+	}
+
+	giveBookByName(bookName) {
+		const book = this.books.find(book => book.name === bookName);
+		if (book) {
+			this.books.splice(this.books.indexOf(book), 1);
+			return book;
+		}
+		return null;
+	}
+}
